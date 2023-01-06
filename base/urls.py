70247351575da7e471dc34d66c6fd608a9e3c1ac
eyal <narnavg@gmail.com>
2023-01-06 10:49:32 +0200

    add refresh

--- base/urls.py
+++ base/urls.py
@@ -2,12 +2,16 @@ from django.urls import path
 from .views import tasks 
 from . import views 
 
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/',views.register ),
     path('tasks/',tasks ),
     path('tasks/<int:id>',tasks),
     path('test/',views.test ),
-    
+    # upload images
 ]
