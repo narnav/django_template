476e0d1f994a8ad6afa568bfc927efdf9076e5a3
eyal <narnavg@gmail.com>
2023-01-06 09:31:39 +0200

    first commit

---
+++ base/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import tasks 
+from . import views 
+
+urlpatterns = [
+    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/',views.register ),
+    path('tasks/',tasks ),
+    path('tasks/<int:id>',tasks),
+    path('test/',views.test ),
+    
+]
