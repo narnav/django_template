45725b9964b17d9e48a1cbcaea0e9736fd10809e
eyal <narnavg@gmail.com>
2023-02-21 19:21:51 +0200

    return to פםדאערקד/ך

--- myproj/settings.py
+++ myproj/settings.py
@@ -118,11 +118,34 @@ WSGI_APPLICATION = 'myproj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        # name = databaseenv
+        'NAME': 'zfania',
+
+        'USER': 'zfania_user',
+
+        'PASSWORD': "nku4iBAt9bX41cvT66cm3pDLdnaksIqh",
+        # hostname +location
+
+        'HOST': 'dpg-cfqf5iarrk08lt73e1eg-a.oregon-postgres.render.com',
+
+        'PORT': "5432"
+
     }
+
 }
 
 
@@ -147,7 +170,6 @@ DATABASES = {
 #     }
 
 # }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
